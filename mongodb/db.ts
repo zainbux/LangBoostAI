f32d00f71f11df1405800aea08952cd2ffd464be
zainbux <zainbux99@gmail.com>
2024-08-09 21:58:24 -0400

    update db info

--- mongodb/db.ts
+++ mongodb/db.ts
@@ -1,4 +1,5 @@
-const connectionString = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@google-translate-clone.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000`;
+const connectionString = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@translatordb.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000`;
+
 
 import mongoose from "mongoose";
 
